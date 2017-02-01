c2eb661f53a113f3018c861ce286105d5fbb3dbd
rsdsantos <rsdsantos@gmail.com>
2017-02-01 00:37:22 -0200

    Nova classe ApiInstaller e novo projeto de Setup.

---
+++ WebAPITopshelf/ApiInstaller.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Configuration.Install;
+
+namespace WebAPITopshelf
+{
+    [RunInstaller(true)]
+    public partial class ApiInstaller : Installer
+    {
+        // Constantes que armazenam uma nova chave no stateSaver contendo o path da instalação do Topshelf.
+        private const string AssemblyIdentifier = "TopshelfAssembly";
+        private const string InstallUtilAssemblyParameter = "assemblypath";
+
+        public ApiInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            var topshelfAssembly = Context.Parameters[InstallUtilAssemblyParameter];
+            stateSaver.Add(AssemblyIdentifier, topshelfAssembly);
+
+            RunHidden(topshelfAssembly, "install");
+
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            var topshelfAssembly = savedState[AssemblyIdentifier].ToString();
+
+            RunHidden(topshelfAssembly, "uninstall");
+
+            base.Uninstall(savedState);
+        }
+
+        /// <summary>
+        /// Executa o processo de instalação ou desinstalação do Topshelf.
+        /// </summary>
+        /// <param name="primaryOutputAssembly">Path do assembly do Topshelf.</param>
+        /// <param name="arguments">Comando a ser executado.</param>
+        private static void RunHidden(string primaryOutputAssembly, string arguments)
+        {
+            var startInfo = new ProcessStartInfo(primaryOutputAssembly)
+            {
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = arguments,
+                UseShellExecute = true
+            };
+
+            using (var process = Process.Start(startInfo))
+            {
+                process.WaitForExit();
+            }
+        }
+    }
+}
